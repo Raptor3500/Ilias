4c0f6440da50bf4913f9a90e1b1f5fe33daa7154
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-12-30 00:18:33 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -149,8 +149,8 @@ async def yt(ctx, url):
     author = ctx.message.author
     voice_channel = author.voice_channel
     vc = await bot.join_voice_channel(voice_channel)
-    player = await vc.create_ytdl_player(url)
-    player.start()
+    
+    await vc.create_ytdl_player(url).start()
       
       
         
