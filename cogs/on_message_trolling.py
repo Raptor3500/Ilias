5ee2c28dd59d082e27db60a7f6096e44edb9bca5
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2019-01-02 15:58:52 -0500

    Create on_message_trolling.py

---
+++ cogs/on_message_trolling.py
@@ -0,0 +1,25 @@
+import discord
+import requests
+import json
+
+user = 'rZuTJlFKDZF5oi0T'
+key = 'jiaN5JDdXrvjRNFng4t9rlMF47pjazst'
+
+class on_message_trolling():
+    def __init__(self, bot):
+        self.bot = bot
+        
+        async def on_message(self, message):
+        user = message.author
+        mesg = message.content
+        print(f'{user} said "{mesg}"')
+        ctxmesg = input()
+        await message.channel.send(ctxmesg)
+        if message.author == self.bot.user:
+            return
+            
+            
+            
+print('Starting Trolling Process...')
+def setup(bot):
+    bot.add_cog(on_message_trolling(bot))
