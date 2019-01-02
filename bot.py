eb2bca6f3e7f830db7867d72c2b2d654ceded739
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2019-01-02 14:14:49 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -171,7 +171,7 @@ async def leave(ctx):
   
       
 @bot.command(pass_context=True)
-async def play(ctx, url,*):
+async def play(ctx, *,url):
   server = ctx.message.server
   author = ctx.message.author
   voice_client = bot.voice_client_in(server)
@@ -197,7 +197,7 @@ async def resume(ctx):
   players[id].resume()
   
 @bot.command(pass_context=True)
-async def queue(ctx, url, *,search):
+async def queue(ctx, *,url,):
   server = ctx.message.server
   voice_client = bot.voice_client_in(server)
   player = await voice_client.create_ytdl_player(url, search, after=lambda: check_queue(server.id))
