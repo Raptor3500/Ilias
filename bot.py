127b63d3905ea3ebcb4fe79fab65bd25db3c4313
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2019-01-07 17:31:02 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -177,17 +177,16 @@ async def leave(ctx):
 async def play(ctx, *,url):
   server = ctx.message.server
   voice_client = bot.voice_client_in(server)
-  if not bot.is_voice_connected(ctx.message.server):
-    await bot.join_voice_channel(ctx.message.author.voioce_channel)
-    try:
-      if players[server.id].is_playing():
-        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
-        queues[server.id] = [player]
-        await bot.say('Video Queued!')
-      else:
-        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
-        player[server.id] = player
-        player.start()
+  await bot.join_voice_channel(ctx.message.author.voioce_channel)
+  try:
+    if players[server.id].is_playing():
+      player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+      queues[server.id] = [player]
+      await bot.say('Video Queued!')
+    else:
+      player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+      player[server.id] = player
+      player.start()
   
 @bot.command(pass_context=True)
 async def pause(ctx):
